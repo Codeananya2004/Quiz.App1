be2b0337c12de4215e056f36a9c38bd28b2df6ae
Ananya Jaiswal <ananyajaiswal.cse23@kgikanpur.in>
2025-02-18 14:31:36 +0530

    first commit

---
+++ app/src/main/java/com/example/quizapp1/QuestionAnswer.java
@@ -0,0 +1,26 @@
+package com.example.quizapp1;
+
+public class QuestionAnswer {
+
+    public static String question[]={
+            "Who is the largest ocean on Earth?",
+            "what is the capital city of Japan?",
+            "Which planet is known as Red Planet?",
+            "which country is famous for the Eiffel tower?",
+            "Who is the first person to walk on the moon?",
+    };
+    public static String choices [][]={
+            {"The Pacific Ocean", "Atlantic Ocean", "Indian Ocean", "Southern Ocean"},
+            {"Kabul", "Tokyo", "Baku", "Canberra"},
+            {"jupiter",  "Saturn", "Earth", "Mars"},
+            {"France", "America", "London", "china"},
+            {"Alan Bean", "Neil Armstrong", "Buzz Aldrin", "Pete Conrad"},
+    };
+    public static String correctAnswers[]={
+            "The Pacific Ocean",
+            "Tokyo",
+            "Mars",
+            "France",
+            "Neil Armstrong",};
+}
+
